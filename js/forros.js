c8fc96467a8de993d0b3c086b5b6b6cb800396e8
Federico del Valle <quaidy@gmail.com>
2013-07-22 15:42:11 -0300

    ajustes en geolocalizacion

--- js/forros.js
+++ js/forros.js
@@ -4,17 +4,18 @@
           // create google maps map
         var mapOptions = {
           zoom: 3,
-          center: new google.maps.LatLng(43, 0),
+          center: new google.maps.LatLng("-34.7235745", "-58.381585"),
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
+        //inicializa mapa
         mapa = new google.maps.Map(document.getElementById('forros-map'), mapOptions);
-        
-          $("#geocomplete").geocomplete({
+        //geocoder
+        $("#geocomplete").geocomplete({
             map: mapa,
             country: 'ar'
-         });
-          var mapeo =  $("#geocomplete").geocomplete("forros-map"); 
-        
+        });
+        var mapeo =  $("#geocomplete").geocomplete("forros-map"); 
+        //agrega layer de cartoDB
         cartodb.createLayer(mapa, 'http://fundhuesped.cartodb.com/api/v2/viz/6e2ce16a-f2ec-11e2-a228-699358bddf8e/viz.json')
         .addTo(mapa)
         .on('done', function(layer) {
